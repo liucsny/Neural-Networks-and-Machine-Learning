c3eefd4f4ced56fda14e6d48565a2b10fcc94db3
liucong <liucong@metrodata.com>
2018-10-25 16:36:17 +0800

    break walls

--- maze-generator/cell.js
+++ maze-generator/cell.js
@@ -14,15 +14,44 @@ function Cell(i, j, w){
 }
 
 Cell.prototype.show = function(){
+  fill(color(225, 0, 225, 150))
+  noStroke();
+  if(this.visited) rect(this.x, this.y, this.w, this.w)
+
   stroke(255);
   noFill();
-
   if(this.wall.top) line(this.x, this.y, this.x + this.w, this.y) //top
   if(this.wall.left) line(this.x, this.y, this.x, this.y + this.w) //left
   if(this.wall.right) line(this.x + this.w, this.y, this.x + this.w, this.y + this.w) //right
   if(this.wall.bottom) line(this.x, this.y + this.w, this.x + this.w, this.y + this.w) //bottom
 
-  fill(color(225, 0, 225))
+}
+
+Cell.prototype.checkNeighbors = function(){
+  let neighbors = [];
+  let top, right, bottom, left;
+
+  if(this.j - 1 >= 0) top = grid[this.i][this.j - 1]
+  if(this.i + 1 < cols) right = grid[this.i + 1][this.j]
+  if(this.j + 1 < rows) bottom = grid[this.i][this.j + 1]
+  if(this.i - 1 >= 0) left = grid[this.i - 1][this.j]
+
+  if(top && !top.visited) neighbors.push(top);
+  if(right && !right.visited) neighbors.push(right);
+  if(bottom && !bottom.visited) neighbors.push(bottom);
+  if(left && !left.visited) neighbors.push(left);
+
+  if(neighbors.length > 0){
+    // console.log(neighbors)
+    let r = neighbors[floor(random(0, neighbors.length))];
+    return r;
+  } else {
+    return undefined
+  }
+}
+
+Cell.prototype.hightLight = function(){
+  fill(color(0, 255, 0))
   noStroke();
-  if(this.visited) rect(this.x, this.y, this.w, this.w)
+  rect(this.x, this.y, this.w, this.w)
 }
--- maze-generator/sketch.js
+++ maze-generator/sketch.js
@@ -1,29 +1,67 @@
 let cols, rows;
-let w = 40;
+let w = 20;
 let grid = [];
 let current;
 
 function setup() {
   createCanvas(800, 800);
+  frameRate(5);
   cols = floor(width/w);
   rows = floor(height/w);
 
-  for (let j = 0; j < cols; j++) {
-    for (let i = 0; i < rows; i++) {
+  for (let i = 0; i < cols; i++) {
+    grid.push([]);
+    for (let j = 0; j < rows; j++) {
       let cell = new Cell(i, j, w);
-      grid.push(cell);
+      grid[i].push(cell);
     }
   }
-  current = grid[0];
+  current = grid[0][0];
   current.visited = true;
-  
+
   console.log('Maze');
+  // console.log(grid);
 }
 
 function draw(){
   background(51);
 
-  grid.forEach(cell=>{
-    cell.show();
+  grid.forEach(array=>{
+    array.forEach(cell=>{
+      cell.show();
+    })
   })
+
+  current.hightLight();
+
+  let next = current.checkNeighbors()
+  // console.log(current)
+  // console.log(next)
+
+  if(next){
+    next.visited = true;
+    removeWalls(current, next);
+    current = next;
+  }
+}
+
+
+function removeWalls(a, b){
+  let x = a.i - b.i;
+  if(x > 0){
+    a.wall.left = false;
+    b.wall.right = false;
+  } else if(x < 0){
+    a.wall.right = false;
+    b.wall.left = false;
+  } else {
+    let y = a.j - b.j;
+    if(y > 0){
+      a.wall.top = false;
+      b.wall.bottom = false;
+    } else if(y < 0){
+      a.wall.bottom = false;
+      b.wall.top = false;
+    }
+  }
 }
