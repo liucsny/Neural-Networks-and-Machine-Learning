e4a14cda6d3d47f142a7fc3df96386d3e3ae85f1
liucong <liucong@metrodata.com>
2019-01-06 14:00:42 +0800

    refactor all nodes to iterate function

--- Session-4 Neural Networks/Multilayer Perceptron/nn.js
+++ Session-4 Neural Networks/Multilayer Perceptron/nn.js
@@ -1,33 +1,41 @@
 class NeuralNetwork {
     constructor(...nodes){
-        this.hidden_layers = [];
-        // console.log(nodes);
-        if (nodes.length < 3){
+        this.nodeArray = nodes;
+        // this.hidden_layers = [];
+        // // console.log(nodes);
+        if (this.nodeArray.length < 3){
             console.log('node layers must be larger than 2')
         } else {
-            for (let i = 1; i < nodes.length - 1; i++) {
-                this.hidden_layers.push(nodes[i]);
+            // for (let i = 1; i < nodes.length - 1; i++) {
+            //     this.hidden_layers.push(nodes[i]);
+            // }
+            // this.input_nodes = nodes[0];
+            // this.output_nodes = nodes[nodes.length - 1];
+            this.weights = [];
+            for (let i = 0; i < this.nodeArray.length - 1; i++) {
+                this.weights.push(new Matrix(this.nodeArray[i + 1], this.nodeArray[i]))
             }
-            this.input_nodes = nodes[0];
-            this.output_nodes = nodes[nodes.length - 1];
+            this.weights.forEach(matrix => {
+                matrix.randomize();
+            })
         }
-        this.weights_ih = new Matrix(this.hidden_layers[0], this.input_nodes);
-        this.weights_hh = [];
-        this.weights_ho = new Matrix(this.output_nodes, this.hidden_layers[this.hidden_layers.length - 1]);
-        this.weights_ih.randomize();
-        this.weights_ho.randomize();
-        for (let i = 0; i < this.hidden_layers.length-1; i++) {
-            this.weights_hh.push(new Matrix(this.hidden_layers[i+1], this.hidden_layers[i]))
-        }
-        this.weights_hh.forEach(matrix => {
-            matrix.randomize();
-        })
+        // this.weights_ih = new Matrix(this.hidden_layers[0], this.input_nodes);
+        // this.weights_hh = [];
+        // this.weights_ho = new Matrix(this.output_nodes, this.hidden_layers[this.hidden_layers.length - 1]);
+        // this.weights_ih.randomize();
+        // this.weights_ho.randomize();
+        // for (let i = 0; i < this.hidden_layers.length-1; i++) {
+        //     this.weights_hh.push(new Matrix(this.hidden_layers[i+1], this.hidden_layers[i]))
+        // }
+        // this.weights_hh.forEach(matrix => {
+        //     matrix.randomize();
+        // })
     }
 
     feedForward(inputArray) {
         //format the input array so that length of input array can match the number of the input nodes
         let format_inputArray = [];
-        for (let i = 0; i < this.input_nodes; i++) {
+        for (let i = 0; i < this.nodeArray[0]; i++) {
             if(inputArray[i] !== undefined ){
                 format_inputArray[i] = inputArray[i];
             } else {
@@ -37,24 +45,25 @@ class NeuralNetwork {
 
         let inputMatrix = Matrix.fromArray(format_inputArray);
 
-        let hiddenInputs = Matrix.mult(this.weights_ih,inputMatrix);
+        // let hiddenInputs = Matrix.mult(this.weights_ih,inputMatrix);
 
-        let hiddenOutputs = this.iterateHiddenFeedForward(hiddenInputs);
+        // let hiddenOutputs = this.iterateFeedForward(hiddenInputs);
 
-        console.log(hiddenOutputs);
-        // result.print()
+        let outputsMatrix = this.iterateFeedForward(inputMatrix);
+
+        console.log(outputsMatrix);
+
     }
 
-    iterateHiddenFeedForward(inputMatrix, index = 0){
-        // console.log(inputMatrix, ' + ', index);
-        let weights = this.weights_hh[index];
-        // console.log(weights);
-        // console.log(inputMatrix);
-        if(weights === undefined){
+    iterateFeedForward(inputMatrix, index = 0){
+        let weight = this.weights[index];
+
+        if(weight === undefined){
             return inputMatrix;
         }
-        let iterateInput = Matrix.mult(weights,inputMatrix);
-        return this.iterateHiddenFeedForward(iterateInput, index + 1);
+        let iterateInput = Matrix.mult(weight,inputMatrix);
+
+        return this.iterateFeedForward(iterateInput, index + 1);
     }
 
 }
--- Session-4 Neural Networks/Multilayer Perceptron/sketch.js
+++ Session-4 Neural Networks/Multilayer Perceptron/sketch.js
@@ -1,7 +1,7 @@
 let brain;
 
 function setup() {
-    brain = new NeuralNetwork(6, 12, 4, 8, 6, 1);
+    brain = new NeuralNetwork(6, 2,5,6,7,9, 9);
     brain.feedForward([2, 3, 4, 5, 7]);
 }
 
